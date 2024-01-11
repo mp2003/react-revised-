58f9d1f9f63ece0b351baec80940cd1261ab1da1
mp2003 <m.p6092003@gmail.com>
2024-01-11 15:48:56 +0530

    adding all the file

--- Guess the number/src/App.tsx
+++ Guess the number/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-
+import { useState, useEffect } from "react";
+import "App.css";
 const App = () => {
   const [value, setValue] = useState("");
   const [score, setScore] = useState(0);
