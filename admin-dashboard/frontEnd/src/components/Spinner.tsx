7a6abdd8c901c7a60ea0c5d7c755bc8b4f28578c
mp2003 <m.p6092003@gmail.com>
2024-01-17 16:26:25 +0530

    set up loading component

--- admin-dashboard/frontEnd/src/components/Spinner.tsx
+++ admin-dashboard/frontEnd/src/components/Spinner.tsx
@@ -9,7 +9,7 @@ const Spinner: React.FC<SpinnerProps> = (props) => {
     <div className="flex h-screen items-center justify-center bg-blue-800 h-20 w-full rounded-lg relative bottom-2">
       {/* Adjust the background color to match the login component */}
       <div className="flex items-center">
-        <span className="loading loading-spinner loading-infinity h-16 w-16 border-t-4 border-white"></span>
+        <span className="loading loading-spinner loading-infinity  border-t-4 border-white"></span>
         {/* Adjust spinner size and border color */}
         <div className="ml-4 text-white">
           {/* Adjust text color to match your design */}
