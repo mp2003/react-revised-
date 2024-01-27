71b78f1846d884cc322139fbc8362e8e73c0f315
mp2003 <m.p6092003@gmail.com>
2024-01-27 22:43:33 +0530

    making new Sections

---
+++ nike/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import SuperQuality from "./sections/SuperQuality";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import CustomerReviews from "./sections/CustomerReviews";
+import Suscribe from "./sections/Suscribe";
+import Footer from "./sections/Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Suscribe,
+  Footer,
+};
