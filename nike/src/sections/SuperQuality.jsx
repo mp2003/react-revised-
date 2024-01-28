68459a5780e2affd9a4ff15c07efcd42a3680ae4
mp2003 <m.p6092003@gmail.com>
2024-01-28 22:04:53 +0530

    removing icons not showing (bugs)

--- nike/src/sections/SuperQuality.jsx
+++ nike/src/sections/SuperQuality.jsx
@@ -22,7 +22,7 @@ const SuperQuality = () => {
           Our dedication to detail and excellence ensures your satisfaction
         </p>
         <div className="mt-11">
-          <Button label="View details" />
+          <Button label="View details" iconURL={""} />
         </div>
       </div>
 
