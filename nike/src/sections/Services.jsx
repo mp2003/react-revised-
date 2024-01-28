c5026c824dd24aee71a8eaa9d91d1b5f18f054af
mp2003 <m.p6092003@gmail.com>
2024-01-28 21:52:46 +0530

    adding popular products

--- nike/src/sections/Services.jsx
+++ nike/src/sections/Services.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { services } from "../constants";
+import { ServiceCard } from "../components";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
